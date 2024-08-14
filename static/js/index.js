22d919bb946674945c05473a1a3409e4443c7a73
GuangyanCai <gcai3@uci.edu>
2024-08-14 17:29:58 +0800

    Added some images.

--- static/js/index.js
+++ static/js/index.js
@@ -1,21 +1,32 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
 
-$(document).ready(function() {
-    // Check for click events on the navbar burger icon
+$(document).ready(function () {
+	// Check for click events on the navbar burger icon
 
-    var options = {
-			slidesToScroll: 1,
-			slidesToShow: 1,
-			loop: true,
-			infinite: true,
-			autoplay: true,
-			autoplaySpeed: 5000,
-    }
+	var options = {
+		slidesToScroll: 1,
+		slidesToShow: 1,
+		loop: true,
+		infinite: true,
+		autoplay: true,
+		autoplaySpeed: 5000,
+	}
 
-		// Initialize all div with carousel class
-    var carousels = bulmaCarousel.attach('.carousel', options);
-	
-    bulmaSlider.attach();
+	// Initialize all div with carousel class
+	var carousels = bulmaCarousel.attach('.carousel', options);
 
+	bulmaSlider.attach();
+
+
+	var nero_cmp_buttons = $('#nero_cmp_buttons > button');
+	nero_cmp_buttons.click(function () {
+		var newWord = $(this).data('word');
+		$('.nero_cmp_imgs').each(function () {
+			var template = $(this).data('template');
+			var newSrc = template.replace(/{{placeholder}}/g, newWord);
+			$(this).attr('src', newSrc);
+		});
+	});
+	nero_cmp_buttons.first().click();
 })
